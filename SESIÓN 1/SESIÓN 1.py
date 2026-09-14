ddedfb824011133d919f0eaf3c90faa3f2ee45d9
DarCamRG <adadaaaaa50@gmail.com>
2026-09-14 01:44:20 +0000

    Correciones Resoluciones de cada Caso

--- SESIÓN 1/SESIÓN 1.py
+++ SESIÓN 1/SESIÓN 1.py
@@ -16,11 +16,9 @@ servidor_estado = {
 
 #Supuesto Motor de Inferencia
 
-#Caso Temperatura Alta
-#temperatura
-
 def diagnosticar_servidor(Funcionamiento):
-    if Funcionamiento ["temperatura"] > 80 and Funcionamiento ["ventilador_apagado"] == False:
+    #Caso Temperatura Alta
+    if Funcionamiento ["temperatura"] > 80 and Funcionamiento.get("ventilador_apagado", False) == True:
         return "El servidor está en riesgo de sobrecalentamiento. Activar ventilador."
 
 
@@ -51,43 +49,50 @@ def diagnosticar_servidor(Funcionamiento):
     elif Funcionamiento["ventilador_encendido"] == True:    
         return "El ventilador del servidor está encendido y funcionando correctamente."
 
-    
 
     #Caso Normal
     else:
-        return "El servidor está funcionando dentro de los parámetros normales."    
+        return "El servidor está funcionando dentro de los parámetros normales." 
+
+
+#-----------------------------------------------------------------------------------------------------
+
+       
 
     print("Caso Normal")
     print("Diagnostico: El servidor está funcionando dentro de los parámetros normales. No se requiere acción adicional.")
+    print(diagnosticar_servidor(servidor_estado))
+
 
     #Caso ping_respuesta
-    print("Caso Todo en orden")
-    caso1 = {"ping_respuesta": 1200}
-    print(diagnosticar_servidor(caso1)) 
+    print("Caso Problemas de Red")
+    caso1 = {temperatura: 40, ventilador_apagado: False, ping_respuesta: 1500}
+    print(diagnosticar_servidor(caso1))  
 
+             
     #Caso cpu_uso y memoria_libre
     print("Caso CPU y Memoria")
-    caso2 = {"cpu_uso": 95, "memoria_libre": 5}
+    caso2 = {"temperatura": 40, "ventilador_apagado": False, "ping_respuesta": 50, "cpu_uso": 95, "memoria_libre": 5}
     print(diagnosticar_servidor(caso2)) 
 
     #Caso memoria_libre
     print("Caso Memoria Baja")
-    caso3 = {"memoria_libre": 8}
+    caso3 = {"temperatura": 40, "ventilador_apagado": False, "ping_respuesta": 50, "cpu_uso": 50, "memoria_libre": 8}
     print(diagnosticar_servidor(caso3)) 
 
     #Caso ventilador_apagado
     print("Caso Ventilador Apagado")
-    caso4 = {"ventilador_apagado": True}
+    caso4 = {"temperatura": 90, "ventilador_apagado": True, "ping_respuesta": 50, "cpu_uso": 50, "memoria_libre": 15}
     print(diagnosticar_servidor(caso4)) 
 
     #Caso ventilador_encendido
     print("Caso Ventilador Encendido")
-    caso5 = {"ventilador_encendido": True}
+    caso5 = {"temperatura": 40, "ventilador_apagado": False, "ping_respuesta": 50, "cpu_uso": 50, "memoria_libre": 15, "ventilador_encendido": True}
     print(diagnosticar_servidor(caso5))
 
     #Caso Sobrecarga
     print("Caso Sobrecarga")
-    Caso6 = {"cpu_uso":100, "memoria_libre": 5, "ping_respuesta":1000, "temperatura":100, "ventilador_apagado": True}
+    Caso6 = {"temperatura": 90, "ventilador_apagado": True, "ping_respuesta": 1500, "cpu_uso": 95, "memoria_libre": 5}
     print(diagnosticar_servidor(Caso6))
 
 
