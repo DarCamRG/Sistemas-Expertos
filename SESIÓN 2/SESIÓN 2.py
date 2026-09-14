d4225eb64322ae01a5c295a244ac8869ecfb2821
DarCamRG <adadaaaaa50@gmail.com>
2026-09-14 02:35:16 +0000

    Base de Reglas R1-R4

--- SESIÓN 2/SESIÓN 2.py
+++ SESIÓN 2/SESIÓN 2.py
@@ -13,5 +13,16 @@ Hechos = {
 }
 
 #Base de Reglas
-Reglas = [
+reglas = [
+    {"id": "R1", "condiciones": {"monto_alto": True}, "conclusion": {"transaccion_inusual": True}},
+    {"id": "R2", "condiciones": {"transaccion_inusual": True, "pais extranjero": True}, "conclusion": {"bloquear_tarjeta": True}},
+    {"id": "R3", "condiciones": {"compra_nocturna": True, "monto_alto": True}, "conclusion": {"alerta_seguridad": True}},
+    {"id": "R4", "condiciones": {"bloquear_tarjeta": True, "alerta_seguridad": True}, "conclusion": {"notificar_cliente": True}},
+
+]
+
+
+
+
+    
 
