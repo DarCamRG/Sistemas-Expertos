cee95f806de2e66fa895c219d1cb7400dc4f1785
DarCamRG <adadaaaaa50@gmail.com>
2026-09-14 01:03:48 +0000

    Caso Ping Alto

--- SESIÓN 1/SESIÓN 1.py
+++ SESIÓN 1/SESIÓN 1.py
@@ -7,22 +7,23 @@
 
 servidor_estado = {
 "cpu_uso": 92, #%
-"memoria_libre": 15 #%
-"ping_respuesta": 320 #ms
-"temperatura": 85 #C°
-"ventilador_apagado": False
-"vemtilador_encendido": True
+"memoria_libre": 15, #%
+"ping_respuesta": 320, #ms
+"temperatura": 85, #C°
+"ventilador_apagado": False,
+"ventilador_encendido": True
 }
 
 #Supuesto Motor de Inferencia
 
 #Caso Temperatura Alta
-#Temperatura
+#temperatura
 
 def diagnosticar_servidor(Funcionamiento):
     if Funcionamiento ["temperatura"] > 80 and Funcionamiento ["ventilador_apagado "] == False:
-        return "El servidor está en riesgo de sobrecalentamiento. Activar ventilador."3
-
+        return "El servidor está en riesgo de sobrecalentamiento. Activar ventilador."
+    
 #Caso Ping Alto Problemas de Red
-#
+#ping_respuesta
+
 
