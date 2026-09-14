7a2e3c40ad1a26cc58da7d9ad2a148816448cb7b
DarCamRG <adadaaaaa50@gmail.com>
2026-09-14 02:25:48 +0000

    SESÍON 2

---
+++ SESIÓN 2/SESIÓN 2.py
@@ -0,0 +1,17 @@
+#SESIÓN 2 — MOTOR DE INFERENCIA Y MODUS PONENS
+#Taller 2 Traza de Inferencia
+
+#Taller Laboratorio - Motor de Fraude Bancario
+#Sistema de Detección de Fraude
+#Base Inicial de Hechos
+
+Hechos = {
+    "monto": 7500,
+    "pais extranjero": True,
+    "compra_nocturna": True,
+    "multiples_intentos": False
+}
+
+#Base de Reglas
+Reglas = [
+
