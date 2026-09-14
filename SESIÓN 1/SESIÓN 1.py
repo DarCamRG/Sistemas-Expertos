4890a8821d978f7f1707d4828627e45eb338b7cd
DarCamRG <adadaaaaa50@gmail.com>
2026-09-14 00:54:42 +0000

    Parte Incial

--- SESIÓN 1/SESIÓN 1.py
+++ SESIÓN 1/SESIÓN 1.py
@@ -1,2 +1,15 @@
 #SESIÓN 1 — INTRODUCCIÓN A SISTEMAS EXPERTOS Y REPASO PYTHON 
 #Taller 1 Logica Proposicional
+
+#Taller Laboratorio - Sistemas de Diagnostico IT
+#Help Desk?
+#Diccionario
+
+servidor_estado = {
+"cpu_uso": 92, #%
+"memoria_libre": 15 #%
+"ping_respuesta": 320 #ms
+"temperatura": 85 #C°
+"ventilador_apagado": False
+}
+
